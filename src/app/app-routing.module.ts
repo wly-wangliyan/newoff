dcb4fa73a1a30a4ecceb883ffb3837aa346454c0
wangliyan <wangliyan@wangliyans-iMac.local>
2020-10-28 09:27:34 +0800

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,45 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NavComponent } from './nav/nav.component';
-import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component'
-const routes:Routes = [
-    {
-        path:'',
-        redirectTo:'/nav',
-        pathMatch:'full'
-    },
-    {
-        path: 'nav',
-        component:NavComponent,
-    },
-    {
-        path: 'login',
-        component:LoginComponent,
-    },
-    {
-        path: 'register',
-        component:RegisterComponent,
-    },
-    {
-        path:'forgotpwd',
-        component:ForgotpwdComponent
-    }
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {NavComponent} from './nav/nav.component';
+import {ForgotpwdComponent} from './forgotpwd/forgotpwd.component';
+import {HomeComponent} from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/nav/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'nav',
+    component: NavComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {path: '**', redirectTo: 'home'}
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'forgotpwd',
+    component: ForgotpwdComponent
+  }
 ];
 
-@NgModule ({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component';
-import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { registerLocaleData } from '@angular/common';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ForgotpwdComponent} from './forgotpwd/forgotpwd.component';
+import {HomeComponent} from './home/home.component';
+import {NavComponent} from './nav/nav.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FooterComponent } from './footer/footer.component';
-import { FootComponent } from './foot/foot.component';
+import {FooterComponent} from './footer/footer.component';
+import {FootComponent} from './foot/foot.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatOptionModule} from '@angular/material/core';
+import {MatMenuModule} from '@angular/material/menu';
 
 registerLocaleData(zh);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,9 +38,12 @@ registerLocaleData(zh);
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    MatSlideToggleModule,
+    MatOptionModule,
+    MatMenuModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  // providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
